febd5fe8ec751b0851fde8c277550652e9946ace
jvd <malekian0079@gmail.com>
2023-10-28 16:35:09 +0330

    add description to item part one

--- js/app.js
+++ js/app.js
@@ -9,21 +9,80 @@ const searchBox = $.querySelector('#searchBox')
 const menu = $.querySelector('#menu')
 const moduleElm = $.querySelector('#module')
 const secTwo = $.querySelector('#secTwo')
+const closeMenuBtn = $.querySelector('#closeMenuBtn')
 let itemFragment = $.createDocumentFragment()
+let flag = true
 let listItems = [1,2,3,4]
 
 
 listItems.forEach(function (item) {
-    listItemsElm.insertAdjacentHTML('beforeend' , '<div class="w-1/3 shadow in rounded m-4 transition-transform ease-in duration-300 delay-100"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-auto rounded bg-gray-200"><path stroke-linecap="round" stroke-linejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" /></svg><div class="flex justify-between px-2 py-1 items-center"><h2>title</h2><span class="text-xs">count: 2</span></div></div>')
+    let container = $.createElement('div')
+    container.className = "w-1/3 shadow rounded m-4 transition-all ease-in duration-300 delay-100 relative "
+    let h2 = $.createElement('h2')
+    h2.className = "bg-white inline-block absolute top-0 left-0 right-0 w-12 mx-auto text-center rounded-b"
+    h2.innerHTML = "title"
+    let svg = $.createElement('div')
+    let description = $.createElement('div')
+    description.className = "flex justify-between px-2 py-1 items-center flex-wrap gap-2"
+    let spanOne = $.createElement('span')
+    spanOne.className = "font-bold text-xs w-1/3"
+    spanOne.innerHTML = "count"
+    let spanTwo = $.createElement('span')
+    spanTwo.className = "text-xs"
+    spanTwo.innerHTML = 2
+    let descriptionTwo = $.createElement("div")
+    descriptionTwo.className = "hidden flex justify-between px-2 py-1 items-center flex-wrap gap-2"
+    descriptionTwo.insertAdjacentHTML('beforeend' , '<span class="text-xs w-1/3"><span class="font-bold">buy</span><br><span class="">25.000</span></span><span class="text-xs w-1/3"><span class="font-bold">sell</span><br><span class="">25.000</span></span><span class="text-xs w-1/3"><span class="font-bold">count</span><br><span class="">2</span></span>')
+    let editBtn = $.createElement('button')
+    editBtn.className = "bg-[#edddfb] py-1 px-2 rounded text-sm"
+    editBtn.innerHTML = "Edit"
+    descriptionTwo.append(editBtn)
+    
+    description.append(spanOne,spanTwo)
+    container.append(svg ,h2 ,description , descriptionTwo)
+    itemFragment.append(container)
+    svg.insertAdjacentHTML('beforeend' , '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-auto rounded bg-[#dcfafe]"><path stroke-linecap="round" stroke-linejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" /></svg>')
+
+    svg.addEventListener('click' , function () {
+        editModeHandler(description , descriptionTwo) 
+    })
+    editBtn.addEventListener('click' , function () {
+        editBtnHandler(container , h2 , description , descriptionTwo , svg) 
+    })
+    
+})
+listItemsElm.append(itemFragment)
+
+function editModeHandler(description , descriptionTwo) {
+    description.classList.toggle('hidden')
+    descriptionTwo.classList.toggle('hidden')
+}
+
+function editBtnHandler(container , h2 , description , descriptionTwo , svg) {
+    console.log(container ,h2);
+    setTimeout(() => {
+        container.classList.add("w-[11.5rem]")
+        h2.classList.remove("absolute")
+        h2.classList.add("text-xl")
+        h2.classList.add("font-bold")
+        svg.className = "w-[50px] p-2 transition-all ease-in duration-300 inline-block"
+    }, 20);
+    description.classList.add('hidden')
+    descriptionTwo.classList.add('hidden')
+}
+
+closeMenuBtn.addEventListener('click' , function (event) {
+    moduleElm.classList.add('hidden')
+    secTwo.classList.remove('blur')
 })
 
-
 function menuHandler(event) {
-    moduleElm.classList.toggle('hidden')
-    moduleElm.classList.toggle('top-0')
-    secTwo.classList.toggle('blur')
+    moduleElm.classList.remove('hidden')
+    moduleElm.classList.add('top-0')
+    secTwo.classList.add('blur')
     setTimeout(() => {
-        moduleElm.classList.toggle('top-1/2')
+
+        moduleElm.classList.add('top-1/2')
     }, 20);
 }
 
